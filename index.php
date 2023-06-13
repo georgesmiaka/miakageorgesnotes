3f27593769551387428203802024dfb74465c97c
gmiak <kayss.g@gmail.com>
2023-06-14 00:59:05 +0200

    Added navbar

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Georgesnotes</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
+    <link href="style/default.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+    <!-- Navbar -->
+    <div class="navbar_div">
+    <?php include 'view/component/navbar.php'?>
+    </div>
+
+    <div class="mainbody_div">
+        <p>MAin </p>
+    </div>
+  </body>
+</html>
---
+++ view/component/navbar.php
@@ -0,0 +1,14 @@
+<nav>
+    <ul>
+        <li class="active"><a href="#home">Home</a></li>
+        <li><a href="#news">Search</a></li>
+        <li><a href="#news">Stories</a></li>
+        <li><a href="#contact">Projects</a></li>
+        <li><a href="#news">Photos</a></li>
+        <li><a href="#news">Books</a></li>
+        <li><a href="#news">Songs</a></li>
+        <li><a href="#news">Movies</a></li>
+        <li><a href="#news">Profile</a></li>
+        <li><a href="#about">More</a></li>
+    </ul>
+</nav>
