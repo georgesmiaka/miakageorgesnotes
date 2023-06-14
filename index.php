0db4bd4572077f3b32504e5222441b4d02af449e
gmiak <kayss.g@gmail.com>
2023-06-14 02:04:17 +0200

    Added icons - navbar

--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Georgesnotes</title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/blog/">
     <link href="style/default.css" rel="stylesheet" type="text/css">
   </head>
   <body>
