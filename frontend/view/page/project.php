4b60d93b01707d47729e8cf03b3074b2428d3845
gmiak <kayss.g@gmail.com>
2023-06-19 02:17:12 +0200

    Project page: done

--- frontend/view/page/project.php
+++ frontend/view/page/project.php
@@ -18,8 +18,108 @@
         <?php include '../component/navbar.php'; ?>
     </div>
     <div class="mainbody_div">
-        <!-- Block1: Wall -->
-        <div class=""></div>
+        <!-- Project1 -->
+        <div class="project_presentation">
+            <p class="project_category">Programming</p>
+            <h1>Memocraft - Virtual Self Communication System</h1>
+            <h5>Exploring the Frontiers of Personalized Interaction:
+                How Memocraft's Virtual Self System Revolutionizes Human-AI Engagement</h5>
+            <p class="project_author_datum">By <b class="project_author">Georges Miaka</b>
+                <spam class="project_datum">June 13, 2023 </spam>
+            </p>
+            <!-- space -->
+            <div class="space"></div>
+            <div class="project_presentation_body">
+                <div class="project_presentation_text article_text">
+                    <h4>Objective:</h4>
+                    <p> The objective of the Memocraft project is to develop a virtual communication system that emulates your
+                        personality and allows you to interact with others as if you were physically present. By leveraging artificial
+                        intelligence and machine learning techniques, Memocraft aims to create a digital representation of yourself capable
+                        of engaging in meaningful conversations and providing responses that align with your characteristics and preferences.
+                    </p>
+
+                    <h4>Description:</h4>
+                    <p>Memocraft is designed to be an advanced AI-driven system that captures and replicates your unique traits, mannerisms,
+                        and communication style. <!--The project involves several key components and steps:--></p>
+
+                    <!--<p><b>Data Collection:</b> To create an accurate virtual version of yourself, the Memocraft system initially requires
+                        a substantial amount of data related to your personality, communication patterns, and preferences. This data can be
+                        gathered from various sources such as text-based conversations, social media posts, emails, or any other relevant digital content.</p>
+
+                    <p><b>Natural Language Processing:</b> Memocraft employs natural language processing (NLP) techniques to analyze and understand the collected textual data.
+                        It utilizes algorithms and models that can extract meaningful insights from the text, including sentiment analysis, topic modeling, and language style
+                        identification.</p>
+
+                    <p><b>Personality Modeling:</b> Based on the extracted insights, Memocraft constructs a comprehensive model of your personality. This model encompasses factors
+                        such as tone, humor, vocabulary, and overall communication style. It aims to capture the nuances that make your interactions unique and genuine.</p>
+
+                    <p><b>Machine Learning:</b> With the personality model in place, Memocraft employs machine learning algorithms to enhance its ability to generate responses that
+                        align with your personality. By training on the collected data, the system learns patterns and correlations between different types of input and appropriate output responses.</p>
+
+                    <p><b>Dialogue Generation:</b> Memocraft employs advanced dialogue generation techniques to generate responses based on incoming queries or conversations.
+                        These techniques include neural language models, recurrent neural networks (RNNs), or even more advanced transformer models such as GPT (Generative Pre-trained Transformer).</p>
+
+                    <p><b>Real-Time Interaction:</b> The Memocraft system provides a user-friendly interface that allows people to communicate with your virtual self in real-time. Users can engage
+                        in text-based conversations or voice interactions, receiving responses that mimic your conversational style and knowledge.</p>
+
+                    <p><b>Continuous Learning:</b> Memocraft is designed to continuously learn and improve over time. It can adapt to changing circumstances, new information, and user feedback.
+                        By incorporating a feedback loop, the system refines its responses, ensuring a more accurate and authentic representation of your personality.</p>-->
+
+                    <p>Overall, Memocraft endeavors to create an AI-powered system that simulates your presence, enabling others to engage in meaningful conversations with a virtual version of yourself.
+                        It combines data analysis, NLP, machine learning, and dialogue generation techniques to provide an immersive and personalized communication experience.</p>
+                </div>
+                <div class="project_presentation_card">
+                    <img src="/frontend/assets/images/memocraft.png" class="card-img-top" alt="image_project">
+                    <div class="card-body">
+                        <p class="card-text article_text">
+                            Memocraft - The Virtual Self Communication System. <br />
+                            Try memocraft <a href="https://github.com/georgesmiaka/memocraft">Github repot</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- space - divider -->
+        <div class="space"></div>
+        <hr class="divider">
+        <div class="space"></div>
+        <!-- Project2 -->
+        <div class="project_presentation">
+            <p class="project_category">Programming</p>
+            <h1>Introducing Qltura: Redefining Social Media for Positive Connections and Mental Well-being</h1>
+            <h5>Discover how Qltura, the groundbreaking social media app, is revolutionizing the digital landscape by fostering inclusivity,
+                combating discrimination, and promoting genuine human connections while prioritizing mental well-being.</h5>
+            <p class="project_author_datum">By <b class="project_author">Georges Miaka</b>
+                <spam class="project_datum">June 13, 2023 </spam>
+            </p>
+            <!-- space -->
+            <div class="space"></div>
+            <div class="project_presentation_body">
+                <div class="project_presentation_text article_text">
+                    <h4>Objective:</h4>
+                    <p> Qltura is a social media app designed to provide a safe and supportive platform for users to share memories,
+                        connect with communities, and promote positive interactions.
+                        The app's primary objective is to combat discrimination, anxiety, misogyny, addiction to social media,
+                        and other negative aspects commonly associated with traditional platforms.
+                        Qltura aims to prioritize user well-being, encourage positive engagement, and foster a sense of belonging within communities.
+                    </p>
+
+                    <h4>Description:</h4>
+                    <p> Qltura's design focuses on delivering a clean, intuitive, and user-friendly interface.
+                        The user experience emphasizes simplicity, enabling seamless sharing of memories and effortless engagement with communities.
+                        Qltura incorporates calming and soothing color schemes to create a serene atmosphere and reduce anxiety-inducing elements.</p>
+                </div>
+                <div class="project_presentation_card">
+                    <img src="/frontend/assets/images/qltura.png" class="card-img-top" alt="image_project">
+                    <div class="card-body">
+                        <p class="card-text article_text">
+                            Qltura - Social Media for Positive Connections and Mental Well-being. <br />
+                            Try qltura <a href="https://github.com/georgesmiaka/memocraft">Github repot</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
     <!-- space -->
     <div class="space_footer"></div>
